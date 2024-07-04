6ca0c40db5a365fb98c893152b753b8a713873e3
NguyenTuong0420 <manhtuonga2bd@Gmail.com>
2024-07-05 03:19:10 +0700

    update reverse_word

--- reverse_vowels.playground/Contents.swift
+++ reverse_vowels.playground/Contents.swift
@@ -30,3 +30,4 @@ func reverseVowels(_ s: String) -> String {
 }
 
 reverseVowels("u'o;B,vKO\"?,.;?fGI 9;`9T`Z,;iqsn4A.:;'H3s8E9. B4TxfOiB'wvM?q'k:Q`J\"E1T7lo e7c!?glI66?JZh\"6 !C,aK! 1J?f9'`SX4Q!Q4XS`'9f?J1 !Ka,C! 6\"hZJ?66Ilg?!c7e ol7T1E\"J`Q:k'q?Mvw'BiOfxT4B .9E8s3H';:.A4nsqi;,Z`To`;o IGf?;.,?\"OKv,B;o'u")
+
--- reverse_words.playground/Contents.swift
+++ reverse_words.playground/Contents.swift
@@ -2,13 +2,10 @@ import Foundation
 
 func reverseWords(_ s: String) -> String {
     var words = s.split(separator: " ")
-    var result = ""
+    words = words.reversed()
+    let result = words.joined(separator: " ")
     
-    for i in stride(from: words.count-1, to: 0, by: -1) {
-        result += words[i] + " "
-    }
-    
-    return result + words[0]
+    return result
 }
 
 reverseWords("a    good     example     ")
